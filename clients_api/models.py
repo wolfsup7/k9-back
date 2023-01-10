16f860e914bba0971bd13bb8f8f8704d5aa70c63
wolfsup7 <laurendooley@gmail.com>
2023-01-10 15:14:46 -0500

    increase picture field length

--- clients_api/models.py
+++ clients_api/models.py
@@ -4,7 +4,7 @@ class Client(models.Model):
     name = models.CharField(max_length=32)
     breed = models.CharField(max_length=32)
     age = models.IntegerField()
-    picture = models.CharField(max_length=100)
+    picture = models.CharField(max_length=250)
     address = models.CharField(max_length=50)
     phone = models.CharField(max_length=32)
     appointment = models.CharField(max_length=32)
